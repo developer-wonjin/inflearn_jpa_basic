ccbb3c922bcdf8c62bf37f7da138686d756c79e5
wonjin <wonjin1776@naver.com>
2022-04-11 13:36:35 +0900

    intial commit

---
+++ src/main/java/jpabasic/ex1hellojpa/Ex1helloJpaApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.ex1hellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1helloJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex1helloJpaApplication.class, args);
+    }
+
+}
